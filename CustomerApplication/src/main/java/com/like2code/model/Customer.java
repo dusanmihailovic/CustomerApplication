4a050b61521a32f41e30e75256020e64558a92d0
Acer Babaroga <Acer Babaroga@DESKTOP-T0EE5KH>
2017-10-30 21:51:17 +0100

    @Entity annotation

--- CustomerApplication/src/main/java/com/like2code/model/Customer.java
+++ CustomerApplication/src/main/java/com/like2code/model/Customer.java
@@ -1,6 +1,15 @@
 package com.like2code.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+	
+	@Id
+	@GeneratedValue
+	private Long customerId;
 	
 	private String firstName;
 	private String lastName;
@@ -10,6 +19,10 @@ public class Customer {
 	public String getCity() {
 		return city;
 	}
+	public Long getCustomerId() {
+		return customerId;
+	}
+	
 	public String getFirstName() {
 		return firstName;
 	}
@@ -21,6 +34,9 @@ public class Customer {
 	}
 	public void setCity(String city) {
 		this.city = city;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
 	}
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
